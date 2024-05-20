03a8ef0bf41685f621adb26fec542ded3f2ba88a
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-20 20:53:16 +0900

    確認用でscとccのページ作成した

---
+++ src/app/cc/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+const ClientComponent = () => {
+  console.log("ClientComponent");
+
+  return (
+    <div>
+      ClientComponent
+    </div>
+  )
+}
+
+export default ClientComponent;
---
+++ src/app/sc/page.tsx
@@ -0,0 +1,11 @@
+
+const ServerComponent = () => {
+    console.log("ServerComponent");
+  return (
+    <div>
+      ServerComponent
+    </div>
+  )
+}
+
+export default ServerComponent;
