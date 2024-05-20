0c14638f996914d11ac8197f7ff67e39e85f7041
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-20 21:13:08 +0900

    タスク情報を取得して一覧ページに表示させた

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -1,8 +1,25 @@
+import { Task } from "@/app/api/tasks/route";
 
-const TaskPage = () => {
+const getAllTasks = async() => {
+  const response = await fetch('http://localhost:3000/api/tasks', {
+    method: 'GET',
+    cache: 'no-store'
+  });
+
+  return await response.json();
+}
+
+const TaskPage = async () => {
+  const tasks = (await getAllTasks()).tasks as Task[];
   return (
     <div>
-      タスク一覧
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <ul>
+            <li>{task.id}: {task.name}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
