f9b1b599a87326a726c69e1801ae6efa7f7be333
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-05-21 14:12:20 +0900

    formの管理を試した

--- src/actions/testActions.ts
+++ src/actions/testActions.ts
@@ -1,8 +1,16 @@
 "use server"
 
-export const createTask = async (taskId: number, formData: FormData) => {
+interface FormState {
+    error: string;
+}
+
+export const createTask = async (taskId: number, state: FormState, formData: FormData) => {
     // DBにtaskを作成
     console.log("タスクを登録しました");
     console.log(formData.get("name"));
     console.log(taskId);
+
+    state.error = 'エラーが発生しました';
+
+    return state;
 }
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,14 +1,30 @@
+'use client'
+
 import { createTask } from "@/actions/testActions"
+import { useFormState, useFormStatus } from "react-dom";
 
 const ServerActionsPage = () => {
     const taskId = 1;
     const createTaskId =  createTask.bind(null, taskId) // bind()でformに存在しないdataをSAに渡す
+    const initialState = { error: ''};
+    const [state, formAction ] =  useFormState(createTaskId, initialState);
+
+    const SubmitButton = () => {
+      const { pending } = useFormStatus();
+      return (
+        <button type='submit' disabled={pending} className='bg-sky-200 disabled:bg:gray-300 ml-2 px-2 rounded-md'>
+          送信
+        </button>
+      );
+    }
+
   return (
     <div>
-      <form action={createTaskId} className='mt-2 flex items-center justify-center'>
+      <form action={formAction} className='mt-2 flex items-center justify-center'>
         <input type="text" name="name" id="name" className='bg-gray-300 rounded-md'/>
-        <button type='submit' className='bg-slate-200 ml-2 px-2 rounded-md'>送信</button>
+        <SubmitButton />
       </form>
+      <div>{state.error}</div>
     </div>
   )
 }
